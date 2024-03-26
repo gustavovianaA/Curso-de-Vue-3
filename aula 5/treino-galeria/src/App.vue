0761cd26042702828a6f94f1763e7f231156334b
Gustavo Viana <gustavow3dev@gmail.com>
2024-03-26 18:05:17 -0300

    aula 5 - parte 1

---
+++ aula 5/treino-galeria/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <Gallery name="Galeria de Montanhas" :selectedImage="selectedImageG1" :images="imagesG1" />
+  <Gallery name="Galeria de Carros" :selectedImage="selectedImageG2" :images="imagesG2" />
+  <Gallery name="Galeria Minimizada de Montanhas" :selectedImage="selectedImageG3" :images="imagesG3" theme="minimized" />
+  <Gallery name="Galeria Minimizada de Carros" :selectedImage="selectedImageG4" :images="imagesG4" theme="minimized" />
+</template>
+
+<script>
+import Gallery from "./components/Gallery.vue";
+
+export default {
+  name: "App",
+  components: {
+    Gallery,
+  },
+  data() {
+    return {
+      selectedImageG1: { title: "Title 1 Test", img: "/img/g1.webp" },
+      imagesG1: [
+        { title: "Title 1 ::", img: "/img/g1.webp" },
+        { title: "Title 2", img: "/img/g2.jpg" },
+        { title: "Title 3", img: "/img/g3.jpg" },
+        { title: "Title 4", img: "/img/g4.jpg" },
+        { title: "Title 5", img: "/img/g5.jpg" },
+        { title: "Title 6", img: "/img/g6.jpg" },
+        { title: "Title 7", img: "/img/g7.webp" },
+        { title: "Title 8", img: "/img/g8.jpg" },
+        { title: "Title 9", img: "/img/g1.webp" },
+        { title: "Title 10", img: "/img/g2.jpg" },
+        { title: "Title 11", img: "/img/g3.jpg" },
+        { title: "Title 12", img: "/img/g4.jpg" },
+        { title: "Title 13", img: "/img/g5.jpg" },
+        { title: "Title 14", img: "/img/g6.jpg" },
+        { title: "Title 15", img: "/img/g7.webp" },
+        { title: "Title 16", img: "/img/g8.jpg" },
+      ],
+      selectedImageG2: { title: "Carro 1", img: "/img/c1.png" },
+      imagesG2: [
+        { title: "Carro 1", img: "/img/c1.png" },
+        { title: "Title 2", img: "/img/c2.jpg" },
+        { title: "Title 3", img: "/img/c3.webp" },
+        { title: "Title 4", img: "/img/c4.jpg" },
+      ],
+      selectedImageG3: { title: "Title 1 Test", img: "/img/g1.webp" },
+      imagesG3: [
+        { title: "Title 1 ::", img: "/img/g1.webp" },
+        { title: "Title 2", img: "/img/g2.jpg" },
+        { title: "Title 3", img: "/img/g3.jpg" },
+        { title: "Title 4", img: "/img/g4.jpg" },
+        { title: "Title 5", img: "/img/g5.jpg" },
+        { title: "Title 6", img: "/img/g6.jpg" },
+        { title: "Title 7", img: "/img/g7.webp" },
+        { title: "Title 8", img: "/img/g8.jpg" },
+        { title: "Title 9", img: "/img/g1.webp" },
+      ],
+      selectedImageG4: { title: "Carro 1", img: "/img/c1.png" },
+      imagesG4: [
+        { title: "Carro 1", img: "/img/c1.png" },
+        { title: "Title 2", img: "/img/c2.jpg" },
+        { title: "Title 3", img: "/img/c3.webp" },
+        { title: "Title 4", img: "/img/c4.jpg" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+body {
+  background: #292323;
+  max-width: 960px;
+  margin: 20px auto;
+}
+</style>
---
+++ aula 5/treino-galeria/src/components/Gallery.vue
@@ -0,0 +1,93 @@
+<template>
+  <div :class="{ minimized: theme === 'minimized' }">
+    <div class="container-gallery">
+      <h2>{{ name }}</h2>
+      <div id="selected-image-title">
+        <h3>{{ selectedImage.title }}</h3>
+      </div>
+      <div id="selected-image">
+        <img :src="selectedImage.img" :alt="selectedImage.title" />
+      </div>
+      <div id="gallery">
+        <div v-for="image in images" class="gallery-item">
+          <img
+            @click="selectImage(image)"
+            :src="image.img"
+            :alt="image.title"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["selectedImage", "images", "name","theme"],
+
+  methods: {
+    selectImage(image) {
+      this.selectedImage.img = image.img;
+      this.selectedImage.title = image.title;
+    },
+  },
+};
+</script>
+
+<style>
+.container-gallery {
+  height: 100vh;
+}
+.minimized .container-gallery{
+    height: auto;
+    width:50%;
+}
+.minimized.container-gallery {
+  height: 50vh;
+}
+.container-gallery h2 {
+  color: #fff;
+}
+#selected-image-title h3 {
+  display: inline-block;
+  padding: 10px 20px;
+  margin: 0;
+  border-radius: 10px 10px 0 0;
+  background: red;
+  color: #fff;
+}
+
+#selected-image img {
+  height: 70vh;
+  width: 100%;
+  margin: 0 auto;
+}
+.minimized #selected-image img{
+    height: 30vh;
+    width: 100%;
+    margin: 0 auto;
+}
+
+#selected-image {
+  margin: 0;
+}
+
+#gallery {
+  display: flex;
+  background: #201e1e;
+  padding: 10px 0;
+  margin: 0;
+  width: 100%;
+  overflow-x: scroll;
+  scrollbar-color: red #201e1e;
+}
+
+.gallery-item {
+  margin: 3px;
+}
+
+.gallery-item img {
+  width: 80px;
+  height: 60px;
+}
+</style>
