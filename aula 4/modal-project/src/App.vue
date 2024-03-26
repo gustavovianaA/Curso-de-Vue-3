04458fa249f9f34eff1a1b6b2e8164184b5749c1
Gustavo Viana <gustavow3dev@gmail.com>
2024-03-26 12:24:06 -0300

    aula 4

---
+++ aula 4/modal-project/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">Click me</button>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      title: "My first Vue App",
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus()
+      
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
---
+++ aula 4/treinobootstrap/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="btn btn-outline-primary">Vue.js + Bootstrap</div>
+    <div class="row my-3" id="content">
+      <div class="col-md-4 bg-danger"></div>
+      <div class="col-md-4 bg-primary"></div>
+      <div class="col-md-4 bg-success"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#content div{
+  height: 300px;
+}
+</style>
